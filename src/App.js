11fa6d3f568d4568582a6ba9566546d2c0639479
nivedita0604 <niveditakumari0604@gmail.com>
2021-07-15 13:59:30 +0530

    added db, pages and nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
-
 import './App.css';
+import Category from './pages/Category';
+import Checkout from './pages/Checkout';
+import Home from './pages/Home';
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div>
+      <div>
+        <Home />
+        <Checkout />
+      </div>
+      <div>
+        <Category />
+      </div>
+    </div>
+  );
 }
 
 export default App;
