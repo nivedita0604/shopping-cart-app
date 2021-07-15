f3d08da00329ea081e27db2a08545ebe30bc53d7
nivedita0604 <niveditakumari0604@gmail.com>
2021-07-15 14:42:25 +0530

    added navbar and define paths

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import NavBar from './components/NavBar';
 import Category from './pages/Category';
 import Checkout from './pages/Checkout';
 import Home from './pages/Home';
 
 function App() {
   return (
-    <div>
-      <div>
-        <Home />
-        <Checkout />
-      </div>
-      <div>
-        <Category />
-      </div>
-    </div>
+    <>
+      <NavBar />
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/category" exact>
+          <Category />
+        </Route>
+        <Route path="/checkout" exact>
+          <Checkout />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+const LINKS = [
+  { link: '/', text: 'Home' },
+  { link: '/category', text: 'Category' },
+  { link: '/checkout', text: 'Checkout' },
+];
 
 const Navbar = () => {
-  return <div>NAv</div>;
+  return (
+    <div>
+      {LINKS.map(({ link, text }) => {
+        return (
+          <div>
+            <Link to={link}>{text}</Link>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Navbar;
